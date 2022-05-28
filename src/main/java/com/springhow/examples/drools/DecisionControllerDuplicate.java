51eb84c1c0e7273ca020c56d3b706752cf489644
Prabhat Yadav <micro@Prabhats-MacBook-Pro.local>
2022-05-28 20:09:41 +0530

    changes

--- src/main/java/com/springhow/examples/drools/DecisionControllerDuplicate.java
+++ src/main/java/com/springhow/examples/drools/DecisionControllerDuplicate.java
@@ -3,15 +3,14 @@ package com.springhow.examples.drools;
 import com.springhow.examples.drools.domain.OrderRequest;
 import com.springhow.examples.drools.helper.CustomKieContainer;
 import com.springhow.examples.drools.helper.KieContainerType;
-import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/controller")
-public class DecisionController {
+@RestController("/controller/duplicate")
+public class DecisionControllerDuplicate {
 
     @Autowired
     private CustomKieContainer customKieContainer;
--- src/test/java/com/springhow/examples/drools/DroolsDemoApplicationTests.java
+++ src/test/java/com/springhow/examples/drools/DroolsDemoApplicationTests.java
@@ -8,6 +8,7 @@ class DroolsDemoApplicationTests {
 
     @Test
     void contextLoads() {
+        
     }
 
 }
